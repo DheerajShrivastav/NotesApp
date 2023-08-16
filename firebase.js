05847b5d2ebda3e8b904774fa35dc5e1ebf8497e
Dheeraj Shrivastav <106469682+DheerajShrivastav@users.noreply.github.com>
2023-08-16 11:15:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, doc } from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: 'AIzaSyCyz82IXt8_tB1S-2u6C634zc9a7O0GaAM',
-  authDomain: 'notes-app-10d04.firebaseapp.com',
-  projectId: 'notes-app-10d04',
-  storageBucket: 'notes-app-10d04.appspot.com',
-  messagingSenderId: '282405688643',
-  appId: '1:282405688643:web:a1c845f0d56aff97fc8bf6',
+  apiKey: 'your-api-key',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 }
 
 // Initialize Firebase
